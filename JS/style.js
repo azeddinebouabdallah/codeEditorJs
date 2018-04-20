8e686d51dfb8d344ab2d59620d858276cf609099
BouabdallahAzeddine <didou.pro.5@gmail.com>
2018-04-20 10:42:59 +0100

    add pages

--- JS/style.js
+++ JS/style.js
@@ -14,11 +14,46 @@ ipcRenderer.on('file', (e, item) => {
   data = item
 
   var filename = data[0].replace(/^.*[\\\/]/, '')
+  var exe = filename.split('.').pop();
   code = JSON.stringify(data, null, 2)
   fs.writeFile('Files/'+ filename +'.json', code, (err) => {console.log(err)})
+  switch (exe) {
+    case 'html':
+    newPage = '<!DOCTYPE html>'+
+    '<html lang="en" dir="ltr">'+
+      '<head>'+
+        '<meta charset="utf-8">'+
+        '<script src="../codemirror/lib/codemirror.js"></script>'+
+        '<link rel="stylesheet" href="../codemirror/lib/codemirror.css">'+
+        '<link rel="stylesheet" href="../codemirror/theme/material.css">'+
+        '<script src="../codemirror/mode/htmlmixed/htmlmixed.js"></script>'+
+        '<title></title>'+
+      '</head>'+
+      '<body>'+
+        '<div id="codeeditor">'+
+        '</div>'+
+        '<script src="./JS/style.js" type="text/javascript"></script>'+
+      '<script type="text/javascript">'+
+        'var myCodeMirror = CodeMirror(document.getElementById(\'codeeditor\'), {'+
+          'lineNumbers: true,'+
+        '  mode: "htmlmixed",'+
+          'theme: "material"'+
+          'value: ' + data[1] +
+        '})'+
+      '</script>'+
+      '</body>'+
+    '</html>'
 
- newPage = '<!DOCTYPE html><html lang="en" dir="ltr"><head><meta charset="utf-8"><title></title></head><body>' +
-  '<textarea>'+ data[1] + '</textarea></body></html>'
+      break;
+    case 'css':
+
+     break;
+
+    default:
+
+  }
+ //newPage = '<!DOCTYPE html><html lang="en" dir="ltr"><head><meta charset="utf-8"><title></title></head><body>' +
+//  '<textarea>'+ data[1] + '</textarea></body></html>'
 
   fs.writeFile('Files/'+ filename, newPage, (err) => {
     console.log(err)
